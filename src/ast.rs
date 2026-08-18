56902dec15d29c5690a8f6f2339926887817ac8f
Lro-Steven-aq <903954856@qq.com>
2026-08-18 16:21:16 +0800

    添加了Parser的部分方法。非可运行提交。

--- src/ast.rs
+++ src/ast.rs
@@ -1,6 +1,6 @@
 pub mod program_structure;
 use program_structure::*;
-use crate::lexer::{TokenType, operators::Operator, symbols::Symbol, types::Type};
+use crate::lexer::{TokenType, keywords::Keyword, literalvalue::Literal, operators::Operator, symbols::Symbol, types::Type};
 
 use super::lexer::Token;
 
@@ -35,7 +35,7 @@ impl Parser {
             }
              */
             self.advance();// 跳过"("
-            let params = self.parse_params();
+            let params = self.parse_params();  // params 是指形参，args是指实参。
             self.expect(Symbol::RParen);  //  ")"
 
             let body = if self.peek() == TokenType::Symbol(Symbol::Semicolon) {
@@ -85,8 +85,228 @@ impl Parser {
 
     fn parse_assign(&mut self) -> Expr {
         let left = self.parse_or();
+
         if self.peek() == TokenType::Operater(Operator::Assign) {
-            self.
+            self.advance();
+            let right = self.parse_assign();
+            Expr::Assign(Box::new(left), Box::new(right))
+        } else {
+            left
+        }
+    }
+
+    /// 解析形参。
+    fn parse_params(&mut self) -> Vec<Param> {
+        let mut params = Vec::new();
+        if self.peek() == TokenType::Symbol(Symbol::RParen) {
+            return params; // 无参。
+        }
+        loop {
+            let _type = self.parse_type();
+            let identifier = self.expect_identifier();
+            params.push(Param { typ: _type, name: identifier });
+
+            match self.peek() {
+                TokenType::Symbol(Symbol::Comma) => {
+                    self.advance();
+                },
+                TokenType::Symbol(Symbol::RParen) => break,
+                _ => panic!("Require ',' or ')'"),
+            }
+        }
+        params
+    }
+
+    fn parse_args(&mut self) -> Vec<Expr> {
+        let mut args = Vec::new();
+
+        if self.peek() == TokenType::Symbol(Symbol::RParen) {
+            return args;
+        }
+
+        loop {
+            args.push(self.parse_expr());
+            match self.peek() {
+                TokenType::Symbol(Symbol::Comma) => {
+                    self.advance();
+                },
+                TokenType::Symbol(Symbol::RParen) => break,
+                _ => panic!("Require ',' or ')'"),
+            }
+        }
+        args
+    }
+    /// 解析一个代码块。
+    /**
+         * ```c
+         * {
+         *   int a;
+         *   float b;
+         *  char ch = 'M';
+         *    }
+         * ```
+    */
+    fn parse_block(&mut self) -> Block {
+        self.expect(Symbol::LBrace);
+        let mut stmts = Vec::new();
+        while self.peek() != TokenType::Symbol(Symbol::RBrace) {
+            stmts.push(self.parse_stmt());  // 当下一个不是"}"时，解析、推入语句。
+            // 拼凑起来！！
+        }
+        self.expect(Symbol::RBrace);
+        Block { stmts }
+    }
+
+    /// 语句解析。
+    /// Important!
+    /// Core!
+    fn parse_stmt(&mut self) -> Stmt {
+        match self.peek() {   // 列举一个代码块里面所可能遇见的关键字。
+            TokenType::Keyword(Keyword::If) => {
+                // if 处理。
+                self.advance();
+                self.expect(Symbol::LParen);
+
+                let condition = self.parse_expr();
+                self.expect(Symbol::RParen);
+
+                let then_stmts = self.parse_stmt();
+                let else_stmts = if self.peek() == TokenType::Keyword(Keyword::Else) {
+                    self.advance();
+                    Some(Box::new(self.parse_stmt()))
+                } else {
+                    None
+                };
+
+                Stmt::If(condition, Box::new(then_stmts), else_stmts)
+            },
+            TokenType::Keyword(Keyword::Return) => {
+                self.advance();                                         //  分号。
+                let expr = if self.peek() != TokenType::Symbol(Symbol::Semicolon) {
+                    Some(self.parse_expr())
+                } else {
+                    None
+                };
+                self.expect(Symbol::Semicolon);
+                Stmt::Return(expr)
+            },
+            TokenType::Symbol(Symbol::LBrace) => {
+                Stmt::Block(self.parse_block())
+            },
+            TokenType::Type(_) => {
+                let decl = self.parse_decl();
+                match decl {
+                    Decl::Varible(var) => Stmt::VaribleDecl(var),
+                    _ => panic!("Expected variables declarations"),
+                }
+            },
+            _ => {
+                // regard as normal expr
+                let expr = self.parse_expr();
+                self.expect(Symbol::Semicolon);
+                Stmt::Expr(expr)
+            },
+
+        }
+    }
+
+    ///////////////////////////////辅助方法，不公开。///////////////////////////////////////
+    
+    ///
+    fn peek(&self) -> TokenType {
+        self.tokens
+            .get(self.position)
+            .map(|T| T.get_token_type().clone())
+            .unwrap_or(TokenType::EOF)
+    }
+
+    /// 向前进一步
+    fn advance(&mut self) {
+        self.position += 1;
+    }
+
+    fn is_eof(&self) -> bool {
+        self.peek() == TokenType::EOF
+    }
+
+    fn expect(&mut self, expected: Symbol) {
+        if self.peek() == TokenType::Symbol(expected.clone()) {
+            self.advance();
+        } else {
+            panic!("Expected {:#?}, got {:#?}.", expected, self.peek());
+        }
+    }
+
+    fn expect_identifier(&mut self) -> String {
+        match self.peek() {
+            TokenType::Identifer(identifier) => {
+                self.advance();
+                identifier
+            },
+            _ => panic!("Expected identifier"),
+        }
+    }
+
+}
+
+
+
+impl Parser {
+    fn parse_or(&mut self) -> Expr {
+        let mut left = self.parse_and();
+
+        while self.peek() == TokenType::Operater(Operator::LogicOr) {
+            self.advance();
+            let right = self.parse_and();
+            left = Expr::BinaryExp(BinaryOp::Or, left, right);
+        }
+        left
+    }
+
+    fn parse_and(&mut self) -> Expr {
+        let mut left = self.parse_eq();
+
+        while self.peek() == TokenType::Operater(Operator::LogicAnd) {
+            self.advance();
+            let right = self.parse_eq();
+            left = Expr::BinaryExp(BinaryOp::Add, Box::new(left), Box::new(right));
+        }
+        left
+    }
+    // TODO: eq,add,mul,unary,primary
+
+
+
+
+
+
+        /// 表达式最基本的单位。
+    fn parse_primary(&mut self) -> Expr {
+        match self.peek() {
+            TokenType::Constant(Literal::Interage(N)) => {
+                self.advance();
+                Expr::Integer(N)
+            },
+            TokenType::Identifer(identifier) => {
+                self.advance();
+                if self.peek() == TokenType::Symbol(Symbol::LParen) {
+                    // 此处即为函数的调用。
+                    self.advance();
+                    let args = self.parse_args();
+                    self.expect(Symbol::RParen);
+                    Expr::CallFunction(identifier, args)                   
+                } else {
+                    Expr::Identifier(identifier)
+                }
+            },
+            TokenType::Symbol(Symbol::LParen) => {
+                self.advance();
+                let expr = self.parse_expr();
+                self.expect(Symbol::RParen);
+                expr
+            },
+            _ => panic!("Unexpected token: {:#?}", self.peek()) // 其他鬼魂直接报错。什么魑魅魍魉管他呢。
+
         }
     }
 }
